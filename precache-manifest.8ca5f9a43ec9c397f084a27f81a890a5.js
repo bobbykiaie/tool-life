3e7ae8398733f38b8d0e83e80c70d1383fbb66aa
Bobby Kiaie <babakkiaie@Babaks-MacBook-Pro.local>
2020-08-07 15:03:54 -0700

    Updates

---
+++ precache-manifest.8ca5f9a43ec9c397f084a27f81a890a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe4ddc0ff0455d484481f4bc51c55b6",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "a059228ad8268e62a55d",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f7b56ec08a98d351a661",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.bbd01dc0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.bbd01dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a059228ad8268e62a55d",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.65773470.chunk.js"
+  },
+  {
+    "revision": "b501eec67c1271769777",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.04a77fec.js"
+  },
+  {
+    "revision": "6d76a09613bd65760507e9f0d6791e03",
+    "url": "/%7BGithub-repo-name%7D/static/media/logo.6d76a096.png"
+  }
+]);
